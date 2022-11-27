72a3259961ee807fb534a188ad5222a3d972e596
JP+ <63192177+joachimpoutaraud@users.noreply.github.com>
2022-11-27 15:53:43 +0100

    Update docstrings

--- udpstream.py
+++ udpstream.py
@@ -247,6 +247,17 @@ class Client:
 
     def evaluate(self):
 
+        # MIT License
+        # Copyright (c) 2021 Chuanyu Xue
+
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        # SOFTWARE.
+
         latency_list = [row[1] for row in results]
         latency_max = max(latency_list)
         latency_avg = sum(latency_list) / len(latency_list)
